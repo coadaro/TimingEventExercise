793bb102b1852ea8693df1721dfff7474e6249ca
“coadaro” <coadaro@gmail.com>
2022-04-13 10:49:06 -0400

    TimingEventsExerciseWithBonus

--- app.js
+++ app.js
@@ -20,32 +20,35 @@ setTimeout(() => {
 }, 2000);
 
 //3a
-const full = setInterval(() => {
-    console.log(`second(s)`);
+let count = 1;
+const stop = setInterval(() => {
+    console.log(count);
+    count++;
 }, 1000);
 
 //3b
-const stop = document.querySelector(`button`);
+const stopButton = document.querySelector(`button`);
 
-stop.addEventListener(`click`, () => {
-    clearInterval(full);
+stopButton.addEventListener(`click`, () => {
+    clearInterval(stop);
   });
 
 //BONUS
 //4
-
-// let num = 12;
-// const countdown = setInterval(() => {
-//     const div3 = document.querySelector(`#countdown`);
-//     const p3 = document.createElement(`p`);
-//     p3.innerText = ``;
-//     div3.append(p3);
-
-//   if (num > 0){
-//     console.log(num);
-//   } else {
-//     console.log(num);
-//     clearInterval(countdown);
-//   }
-//   num--;
-// }, 1000);
+const div3 = document.querySelector(`#countdown`);
+const p3 = document.createElement(`p`);
+let num = 120;
+const count2 = setInterval(() => {
+    if (num > 0){
+      let minutes = Math.floor(num/60);
+      let seconds = num - minutes * 60;
+      p3.innerText = `${minutes}:${seconds}`;
+      div3.append(p3);
+    }
+    else {
+      clearInterval(count2);
+      p3.innerText = `TIME IS UP!`;
+      div3.append(p3);
+    }
+    num--;
+}, 1000);
